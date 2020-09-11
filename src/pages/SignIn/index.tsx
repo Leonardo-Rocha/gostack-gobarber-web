0db2e7874afea9ddb3fff72f04d6ccf75365e234
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-09-11 15:39:12 -0400

    Add uuid and react-spring packages Refactor hooks to AppProvider Add Toast with animations and respective components Rename auth hook Add toast hook Change signIn to use toast hook

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,7 +9,8 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 import { Container, Content, Background } from './styles';
 
@@ -22,6 +23,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -38,16 +40,24 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Verifique se as credenciais estão corretas.',
+        });
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
